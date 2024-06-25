bd61e9c7f08ec137d09a963b7e4385bc59b2fb34
Arthur <temp2003late@gmail.com>
2024-06-25 23:07:01 +0300

    Finish small bot

--- telebot/telebot_main.py
+++ telebot/telebot_main.py
@@ -9,10 +9,23 @@ def start_bot(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
     btn1 = types.KeyboardButton('Перейти на сайт')
     btn2 = types.KeyboardButton('Видалити фото')
-    btn3 = types.KeyboardButton('Змінити текст')
+    btn3 = types.KeyboardButton('...')
     markup.row(btn1)
     markup.row(btn2, btn3)
     TOKEN.send_message(message.chat.id, f'Привіт, {message.from_user.first_name}!', reply_markup=markup)
+    file_photo = open('./telebot/photo/photo.jpg', 'rb')
+    TOKEN.send_photo(message.chat.id, file_photo, reply_markup=markup)
+    TOKEN.register_next_step_handler(message, on_click)
+
+def on_click(message):
+    if message.text == 'Перейти на сайт':
+        TOKEN.send_message(message.chat.id, 'Сайт відкрито!')
+    elif message.text == 'Видалити фото':
+        TOKEN.send_message(message.chat.id, 'Фото видалено!')
+    elif message.text == 'Змінити текст':
+        TOKEN.send_message(message.chat.id, 'Я поставив три крапочки)))')
+    else:
+        TOKEN.send_message(message.chat.id, 'Я тебе не розумію(')
 
 # Кнопки під фото
 @TOKEN.message_handler(content_types=['photo'])
@@ -20,7 +33,7 @@ def get_photo(message):
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton('Перейти на сайт', url='https://youtube.com')
     btn2 = types.InlineKeyboardButton('Видалити фото', callback_data='delete')
-    btn3 = types.InlineKeyboardButton('...', callback_data='edit')
+    btn3 = types.InlineKeyboardButton('Ще не готово', callback_data='edit')
     markup.row(btn1)
     markup.row(btn2, btn3)
     TOKEN.reply_to(message, 'Яке чудове фото!', reply_markup=markup)
@@ -31,6 +44,5 @@ def callback_message(callback):
         TOKEN.delete_message(callback.message.chat.id, callback.message.message_id - 1)
     elif callback.data == 'edit':
         TOKEN.edit_message_text('Змінити текст', callback.message.chat.id, callback.message.message_id)
-
 
 TOKEN.polling(non_stop=True)
